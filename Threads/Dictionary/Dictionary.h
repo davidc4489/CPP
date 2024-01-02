d09056cce78d70c5852ff27f6efef5e15549cae3
davidc4489 <davidc4489@gmail.com>
2024-01-02 09:52:00 +0200

    LockGard

--- Threads/Dictionary/Dictionary.h
+++ Threads/Dictionary/Dictionary.h
@@ -6,16 +6,18 @@
 #include <random>
 
 std :: shared_mutex mutex_ ; 
+// std :: mutex mutex_ ; 
 
 template <typename T, typename U>
 class Dictionary{
-protected:
+public:
     std::map <T, U> map;
     int size;
 
 public:
     Dictionary():size{0}{}
     U find(T key) {
+        // std:: unique_lock lock ( mutex_ ) ; 
         std:: shared_lock lock ( mutex_ ) ;
         auto it = map.find(key);
         if (it != map.end()) {
@@ -28,8 +30,10 @@ public:
     }
     void insert(T key, U value){
         std:: unique_lock lock ( mutex_ ) ; 
+        if(map.find(key) == map.end()){
+            size++;
+        }
         map.insert(std::pair<T, U>(key, value));
-        size++;
     }
 
     void insertMultiplyValues(U value, int KeysNumber){
@@ -43,12 +47,30 @@ public:
         (void) randomNumber;
 
         for(i = 0; i < KeysNumber; i++) {
-            /* randomNumber = distribution(gen); */
-            insert(i, value);
+            randomNumber = distribution(gen);
+            insert(randomNumber, value);
         }
     }
 
+    void sumFounds(int KeysNumber){
+        // std:: unique_lock lock ( mutex_ ) ; 
+        // std::shared_lock lock ( mutex_ ) ; 
+        std::random_device rd;  
+        std::mt19937 gen(rd()); 
+        std::uniform_int_distribution<int> distribution(0, 1000000);
+        int sum = 0;
+        for(int i = 0; i < KeysNumber; i++) {
+            int randomNumber = distribution(gen);
+            if(map.find(randomNumber)!= map.end()) {
+                // std::cout << "Find ! " << map[randomNumber] << std::endl;
+                sum += map[randomNumber];
+            }
+        }
+        std::cout << "Sum of founds: " << sum << std::endl;
+    }
+
     void print(){
+        // std:: unique_lock lock ( mutex_ ) ; 
         std::shared_lock lock ( mutex_ ) ;
         for(auto it = map.begin(); it!= map.end(); it++) {
             std::cout << it->first << " : " << it->second << std::endl;
@@ -56,6 +78,7 @@ public:
     }
 
     long getSize() const{
+        // std:: unique_lock lock ( mutex_ ) ; 
         std:: shared_lock lock ( mutex_ ) ;
         return size;
     }
--- Threads/Dictionary/main.cpp
+++ Threads/Dictionary/main.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 #include <random>
 #include <thread>
+#include <chrono>
 
 int main(){
 
-    /*  Générer un nombre aléatoire */
     Dictionary<int, int> dic;
+
+    auto start_time = std::chrono::high_resolution_clock::now();
 
     std::cout << "Size :" << dic.getSize() << std::endl;
 
@@ -21,13 +23,60 @@ int main(){
     std::cout << dic.find(1) << std::endl;
     std::cout << dic.find(3) << std::endl;
 
-    std::thread first(&Dictionary<int, int>::insertMultiplyValues, &dic,3, 1000);
-    std::thread second(&Dictionary<int, int>::insertMultiplyValues, &dic,5, 1000);
+    std::thread first(&Dictionary<int, int>::insertMultiplyValues, &dic,3, 300000);
+    std::thread second(&Dictionary<int, int>::insertMultiplyValues, &dic,5, 300000);
+    std::thread third(&Dictionary<int, int>::insertMultiplyValues, &dic,7, 3000000);
+    
     first.join(); 
     second.join();
+    third.join();
 
+    std::thread fourth(&Dictionary<int, int>::sumFounds, &dic,500);
+    std::thread fifth(&Dictionary<int, int>::sumFounds, &dic,500);
+    std::thread sixth(&Dictionary<int, int>::sumFounds, &dic,500);
+    std::thread seventh(&Dictionary<int, int>::sumFounds, &dic,500);
+    std::thread eighth(&Dictionary<int, int>::sumFounds, &dic,500);
+    std::thread ninth(&Dictionary<int, int>::sumFounds, &dic,500);
+    std::thread tenth(&Dictionary<int, int>::sumFounds, &dic,500);
+    std::thread eleventh(&Dictionary<int, int>::sumFounds, &dic,500);
+    std::thread twelfth(&Dictionary<int, int>::sumFounds, &dic,500);
+    std::thread thirteenth(&Dictionary<int, int>::sumFounds, &dic,500); 
+    std::thread fourteenth(&Dictionary<int, int>::sumFounds, &dic,5000);
+    std::thread fifteenth(&Dictionary<int, int>::sumFounds, &dic,5000);
+
+    fourth.join();
+    fifth.join();
+    sixth.join();
+    seventh.join();
+    eighth.join();
+    ninth.join();
+    tenth.join();
+    eleventh.join();
+    twelfth.join();
+    thirteenth.join();
+    fourteenth.join();
+    fifteenth.join();
+
+    // dic.insertMultiplyValues(3, 300000);
+    // dic.insertMultiplyValues(5, 300000);
+    // dic.insertMultiplyValues(7, 3000000);
+    // dic.sumFounds(1000);
+    // dic.sumFounds(1000);
+    // dic.sumFounds(1000);
+    // dic.sumFounds(1000);
+    // dic.sumFounds(1000);
+    // dic.sumFounds(10000);
+
+    // dic.print();
     std::cout << "Size :" << dic.getSize() << std::endl;
-    dic.print();
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+
+    // Calculez la durée d'exécution en millisecondes
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+
+    // Affichez la durée d'exécution
+    std::cout << "Temps d'exécution : " << duration.count() << " millisecondes" << std::endl;
 
     return 0;
 }
